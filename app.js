df9e1b51027bafb02397279a9a10c9326875bfe4
Loc Do <baolocdo@yahoo.com>
2015-04-17 21:57:16 -0700

    Serves html directly

--- app.js
+++ app.js
@@ -3,13 +3,14 @@
  * Module dependencies
  */
 
-var express = require('express'),
-  methodOverride = require('method-override'),
-  morgan = require('morgan'),
-  routes = require('./routes'),
-  api = require('./routes/api'),
-  http = require('http'),
-  path = require('path');
+var express = require('express');
+var methodOverride = require('method-override');
+var morgan = require('morgan');
+var routes = require('./routes');
+var api = require('./routes/api');
+var http = require('http');
+var path = require('path');
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 
 var app = module.exports = express();
 
@@ -22,8 +23,13 @@ var app = module.exports = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(morgan('dev'));
+
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 var env = process.env.NODE_ENV || 'development';
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@
  */
 
 exports.index = function(req, res){
-  res.render('index');
+  //res.render('index');
+  res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 };
 
 exports.partials = function (req, res) {
